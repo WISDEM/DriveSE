aa3229e6f9efe60815d4f9968bcdfcbcda5aac57
tparsons <taylor.parsons@nrel.gov>
2015-03-25 15:07:42 -0600

    modified hub for cm and inertia dependency avoidance

--- src/drivese/hub.py
+++ src/drivese/hub.py
@@ -11,10 +11,98 @@ import numpy as np
 from math import pi, cos, sqrt, radians, sin, exp, log10, log, floor, ceil
 
 from fusedwind.interface import implement_base
-from drivewpact.hub import HubBase, HubSystemAdder
+from drivewpact.hub import HubBase#, HubSystemAdder
 from drivese.drivese_utils import get_L_rb
 
 @implement_base(HubBase)
+class Hub_System_Adder_drive(Component):
+    ''' Get_hub_cm class
+          The Get_hub_cm class is used to pass the hub cm data to upper level models.
+          It contains the general properties for a wind turbine component as well as additional design load and dimentional attributes as listed below.
+    '''
+
+    # variables
+    rotor_diameter = Float(iotype='in', units='m', desc='rotor diameter')
+    L_rb = Float(0.0,iotype='in', units = 'm', desc = 'distance between hub center and upwind main bearing')
+    shaft_angle = Float(iotype = 'in', units = 'deg', desc = 'shaft angle')
+    MB1_location = Array(iotype = 'in', units = 'm', desc = 'center of mass of main bearing in [x,y,z] for an arbitrary coordinate system')
+    hub_mass = Float(iotype='in', units='kg',desc='mass of Hub')
+    hub_diameter = Float(3.0,iotype='in', units='m', desc='hub diameter')
+    hub_thickness = Float(iotype='in', units='m', desc='hub thickness')
+    pitch_system_mass = Float(iotype='in', units='kg',desc='mass of Pitch System')
+    spinner_mass = Float(iotype='in', units='kg',desc='mass of spinner')
+
+    # outputs
+    hub_system_cm = Array(iotype='out', desc='center of mass of the hub relative to tower to in yaw-aligned c.s.')
+    hub_system_I = Array(iotype='out', desc='mass moments of Inertia of hub [Ixx, Iyy, Izz, Ixy, Ixz, Iyz] around its center of mass in yaw-aligned c.s.')
+    hub_system_mass = Float(iotype='out', units='kg',desc='mass of hub system')
+
+    def __init__(self):
+        ''' Initialize Get_hub_cm component
+        '''
+
+        super(GetHubCM_drive, self).__init__()
+
+        #controls what happens if derivatives are missing
+        self.missing_deriv_policy = 'assume_zero'
+
+    def execute(self):
+        if self.L_rb>0:
+            L_rb = self.L_rb
+        else:
+            L_rb = get_L_rb(self.rotor_diameter)
+
+        cm = np.array([0.0,0.0,0.0])
+        cm[0]     = self.MB1_location[0] - L_rb
+        cm[1]     = 0.0
+        cm[2]     = self.MB1_location[2] + L_rb*sin(radians(self.gamma))
+        self.hub_system_cm = (cm)
+
+        self.hub_system_mass = self.hub_mass + self.pitch_system_mass + self.spinner_mass
+
+        #add I definitions here
+        hub_I = np.array([0.0, 0.0, 0.0])
+        hub_I[0] = 0.4 * (self.hub_mass) * ((self.hub_diameter / 2) ** 5 - (self.hub_diameter / 2 - self.hub_thickness) ** 5) / \
+               ((self.hub_diameter / 2) ** 3 - (self.hub_diameter / 2 - self.hub_thickness) ** 3)
+        hub_I[1] = hub_I[0]
+        hub_I[2] = hub_I[1]
+
+        pitch_system_I = np.array([0.0, 0.0, 0.0])
+        pitch_system_I[0] = self.pitch_system_mass * (self.diameter ** 2) / 4
+        pitch_system_I[1] = pitch_system_I[0]
+        pitch_system_I[2] = pitch_system_I[1]
+
+
+        if self.hub_diameter == 0:
+            spinner_diameter =(3.30)
+        else:
+            spinner_diameter =(self.hub_diameter)
+        spinner_thickness = spinner_diameter * (0.055 / 3.30)         # 0.055 for 1.5 MW outer diameter of 3.3 - using proportional constant
+
+        spinner_I = np.array([0.0, 0.0, 0.0])
+        spinner_I[0] = 0.4 * (self.spinner_mass) * ((spinner_diameter / 2) ** 5 - (spinner_diameter / 2 - spinner_thickness) ** 5) / \
+               ((spinner_diameter / 2) ** 3 - (spinner_diameter / 2 - spinner_thickness) ** 3)
+        spinner_I[1] = spinner_I[0]
+        spinner_I[2] = spinner_I[1]
+
+
+        #add moments of inertia
+        I = np.zeros(6)
+        for i in (range(0,3)):                        # calculating MOI, at nacelle center of gravity with origin at tower top center / yaw mass center, ignoring masses of non-drivetrain components / auxiliary systems
+            # calculate moments around CM
+            # sum moments around each components CM
+            I[i]  =  hub_I[i] + pitch_system_I[i] + spinner_I[i]
+            # translate to hub system CM using parallel axis theorem- because cm is assumed shared, unneeded
+            # for j in (range(0,3)):
+            #     if i != j:
+            #         I[i] +=  (self.hub_mass * (self.hub_cm[j] - self.hub_system_cm[j]) ** 2) + \
+            #                       (self.pitch_system_mass * (self.pitch_system_cm[j] - self.hub_system_cm[j]) ** 2) + \
+            #                       (self.spinner_mass * (self.spinner_cm[j] - self.hub_system_cm[j]) ** 2)
+        self.hub_system_I = I
+
+#-------------------------------------------------------------------------------
+
+
 class HubSE(Assembly):
     '''
        HubWPACT class
@@ -26,19 +114,12 @@ class HubSE(Assembly):
     rotor_bending_moment = Float(iotype='in', units='N*m', desc='flapwise bending moment at blade root')
     rotor_diameter = Float(iotype='in', units='m', desc='rotor diameter')
     blade_root_diameter = Float(iotype='in', units='m', desc='blade root diameter')
-    L_rb = Float(iotype='in', units = 'm', desc = 'distance between hub center and upwind main bearing')
-    gamma = Float(iotype = 'in', units = 'deg', desc = 'shaft angle')
-    MB1_location = Array(iotype = 'in', units = 'm', desc = 'center of mass of main bearing in [x,y,z] for an arbitrary coordinate system')
     machine_rating = Float(iotype = 'in', units = 'MW', desc = 'machine rating of turbine')
     
     # parameters
     blade_number = Int(3, iotype='in', desc='number of turbine blades')
 
     # outputs
-    hub_system_mass = Float(0.0, iotype='out', units='kg', desc='overall component mass')
-    hub_system_cm = Array(iotype='out', desc='center of mass of the hub relative to tower to in yaw-aligned c.s.')
-    hub_system_I = Array(iotype='out', desc='mass moments of Inertia of hub [Ixx, Iyy, Izz, Ixy, Ixz, Iyz] around its center of mass in yaw-aligned c.s.')
-
     hub_mass = Float(0.0, iotype='out', units='kg')
     pitch_system_mass = Float(0.0, iotype='out', units='kg')
     spinner_mass = Float(0.0, iotype='out', units='kg')
@@ -52,35 +133,22 @@ class HubSE(Assembly):
         self.add('spinner', Spinner_drive())
 
         # workflow
-        self.driver.workflow.add(['hubSystem', 'hub', 'pitchSystem', 'spinner'])
+        self.driver.workflow.add(['hub', 'pitchSystem', 'spinner'])
 
         # connect inputs
         self.connect('blade_mass', ['pitchSystem.blade_mass'])
         self.connect('rotor_bending_moment', ['pitchSystem.rotor_bending_moment'])
         self.connect('blade_number', ['hub.blade_number', 'pitchSystem.blade_number'])
         self.connect('rotor_diameter', ['hub.rotor_diameter', 'pitchSystem.rotor_diameter', 'spinner.rotor_diameter'])
-        self.connect('hub.diameter', ['pitchSystem.hub_diameter', 'spinner.hub_diameter'])
         self.connect('blade_root_diameter', 'hub.blade_root_diameter')
-        self.connect('L_rb',['hub.L_rb','pitchSystem.L_rb','spinner.L_rb'])
-        self.connect('gamma',['hub.gamma','pitchSystem.gamma','spinner.gamma'])
-        self.connect('MB1_location',['hub.MB1_location','pitchSystem.MB1_location','spinner.MB1_location'])
         self.connect('machine_rating','hub.machine_rating')
 
-        # connect components
-        self.connect('hub.mass', 'hubSystem.hub_mass')
-        self.connect('hub.cm', 'hubSystem.hub_cm')
-        self.connect('hub.I', 'hubSystem.hub_I')
-        self.connect('pitchSystem.mass', 'hubSystem.pitch_system_mass')
-        self.connect('pitchSystem.cm', 'hubSystem.pitch_system_cm')
-        self.connect('pitchSystem.I', 'hubSystem.pitch_system_I')
-        self.connect('spinner.mass', 'hubSystem.spinner_mass')
-        self.connect('spinner.cm', 'hubSystem.spinner_cm')
-        self.connect('spinner.I', 'hubSystem.spinner_I')
+        # # connect components #removed and connected at turbine level
+        # self.connect('hub.mass', 'hubSystem.hub_mass')
+        # self.connect('pitchSystem.mass', 'hubSystem.pitch_system_mass')
+        # self.connect('spinner.mass', 'hubSystem.spinner_mass')
 
         # connect outputs
-        self.connect('hubSystem.hub_system_mass', 'hub_system_mass')
-        self.connect('hubSystem.hub_system_cm', 'hub_system_cm')
-        self.connect('hubSystem.hub_system_I', 'hub_system_I')
         self.connect('hub.mass', 'hub_mass')
         self.connect('pitchSystem.mass', 'pitch_system_mass')
         self.connect('spinner.mass', 'spinner_mass')
@@ -98,7 +166,6 @@ class Hub_drive(Component):
 
     # variables
     blade_root_diameter = Float(iotype='in', units='m', desc='blade root diameter')
-    MB1_location = Array(iotype = 'in', units = 'm', desc = 'center of mass of main bearing in [x,y,z] for an arbitrary coordinate system')
     machine_rating = Float(iotype = 'in', units = 'MW', desc = 'machine rating of turbine')
     
     # parameters
@@ -106,8 +173,8 @@ class Hub_drive(Component):
 
     # outputs
     diameter = Float(0.0, iotype='out', units='m', desc='hub diameter')
+    thickness = Float(0.0, iotype='out',units='m',desc='hub thickness')
     mass = Float(0.0, iotype='out', units='kg', desc='overall component mass')
-    I = Array(np.array([0.0, 0.0, 0.0]), iotype='out', desc=' moments of Inertia for the component [Ixx, Iyy, Izz] around its center of mass')
     
     def __init__(self):
         ''' 
@@ -140,13 +207,6 @@ class Hub_drive(Component):
         self.diameter=2*rCyl
         self.thickness=castThickness
 
-        I = np.array([0.0, 0.0, 0.0])
-
-        I[0] = 0.4 * (self.mass) * ((self.diameter / 2) ** 5 - (self.diameter / 2 - self.thickness) ** 5) / \
-               ((self.diameter / 2) ** 3 - (self.diameter / 2 - self.thickness) ** 3)
-        I[1] = I[0]
-        I[2] = I[1]
-        self.I = (I)
 
 class PitchSystem_drive(Component):
     '''
@@ -160,18 +220,12 @@ class PitchSystem_drive(Component):
     blade_mass = Float(iotype='in', units='kg', desc='mass of one blade')
     rotor_bending_moment = Float(iotype='in', units='N*m', desc='flapwise bending moment at blade root')
     rotor_diameter = Float(iotype='in', units='m', desc='rotor diameter')
-    MB1_location = Array(iotype = 'in', units = 'm', desc = 'center of mass of main bearing in [x,y,z] for an arbitrary coordinate system')
-    L_rb = Float(iotype='in', units = 'm', desc = 'distance between hub center and upwind main bearing')
-    gamma = Float(iotype = 'in', units = 'deg', desc = 'shaft angle')
 
     # parameters
-    hub_diameter = Float(0.0, iotype='in', units='m', desc='hub diameter')
     blade_number = Int(3, iotype='in', desc='number of turbine blades')
 
     # outputs
     mass = Float(0.0, iotype='out', units='kg', desc='overall component mass')
-    cm = Array(np.array([0.0, 0.0, 0.0]), iotype='out', desc='center of mass of the component in [x,y,z] for an arbitrary coordinate system')
-    I = Array(np.array([0.0, 0.0, 0.0]), iotype='out', desc=' moments of Inertia for the component [Ixx, Iyy, Izz] around its center of mass')
 
     def __init__(self):
         '''
@@ -200,29 +254,6 @@ class PitchSystem_drive(Component):
         self.mass =hubpitchFact * (0.22 * self.blade_mass * self.blade_number + 12.6 * self.rotor_bending_moment * (pitchmatldensity / pitchmatlstress))
                                                             # mass of pitch system based on Sunderland model
 
-        # calculate mass properties
-        if self.hub_diameter == 0:
-            self.diameter =(3.30)
-        else:
-            self.diameter =(self.hub_diameter)
-
-        if self.L_rb:
-            L_rb = self.L_rb
-        else:
-            L_rb = get_L_rb(self.rotor_diameter)
-
-        cm = np.array([0.0,0.0,0.0])
-        cm[0]     = self.MB1_location[0] - L_rb
-        cm[1]     = 0.0
-        cm[2]     = self.MB1_location[2] + L_rb*sin(radians(self.gamma))
-        self.cm = (cm)
-
-        I = np.array([0.0, 0.0, 0.0])
-        I[0] = self.mass * (self.diameter ** 2) / 4
-        I[1] = I[0]
-        I[2] = I[1]
-        self.I = (I)
-
 #-------------------------------------------------------------------------------
 
 class Spinner_drive(Component):
@@ -235,17 +266,9 @@ class Spinner_drive(Component):
 
     # variables
     rotor_diameter = Float(iotype='in', units='m', desc='rotor diameter')
-    MB1_location = Array(iotype = 'in', units = 'm', desc = 'center of mass of main bearing in [x,y,z] for an arbitrary coordinate system')
-    L_rb = Float(iotype='in', units = 'm', desc = 'distance between hub center and upwind main bearing')
-    gamma = Float(iotype = 'in', units = 'deg', desc = 'shaft angle')
-
-    # parameters
-    hub_diameter = Float(0.0, iotype='in', units='m', desc='hub diameter')
 
     # outputs
     mass = Float(0.0, iotype='out', units='kg', desc='overall component mass')
-    cm = Array(np.array([0.0, 0.0, 0.0]), iotype='out', desc='center of mass of the component in [x,y,z] for an arbitrary coordinate system')
-    I = Array(np.array([0.0, 0.0, 0.0]), iotype='out', desc=' moments of Inertia for the component [Ixx, Iyy, Izz] around its center of mass')
 
     def __init__(self):
         '''
@@ -260,31 +283,6 @@ class Spinner_drive(Component):
     def execute(self):
 
         self.mass =18.5 * self.rotor_diameter + (-520.5)   # spinner mass comes from cost and scaling model
-
-        # calculate mass properties
-        if self.hub_diameter == 0:
-            self.diameter =(3.30)
-        else:
-            self.diameter =(self.hub_diameter)
-        self.thickness = self.diameter * (0.055 / 3.30)         # 0.055 for 1.5 MW outer diameter of 3.3 - using proportional constant
-
-        if self.L_rb:
-            L_rb = self.L_rb
-        else:
-            L_rb = get_L_rb(self.rotor_diameter)
-
-        cm = np.array([0.0,0.0,0.0])
-        cm[0]     = self.MB1_location[0] - L_rb
-        cm[1]     = 0.0
-        cm[2]     = self.MB1_location[2] + L_rb*sin(radians(self.gamma))
-        self.cm = (cm)
-
-        I = np.array([0.0, 0.0, 0.0])
-        I[0] = 0.4 * (self.mass) * ((self.diameter / 2) ** 5 - (self.diameter / 2 - self.thickness) ** 5) / \
-               ((self.diameter / 2) ** 3 - (self.diameter / 2 - self.thickness) ** 3)
-        I[1] = I[0]
-        I[2] = I[1]
-        self.I = (I)
 
 
 def example_5MW_4pt():
